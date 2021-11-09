9f9dad49a1c9cbf6701080ae7e5d027597c8201e
YMashina <lethanavir@gmail.com>
2021-11-09 18:19:27 +0300

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className={styles.appContent}>
       <SelectAddress />
-      {address ? <Residents /> : null}
+      {address && <Residents />}
     </div>
   );
 };
